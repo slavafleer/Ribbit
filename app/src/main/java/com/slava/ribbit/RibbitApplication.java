a5366af8d35201ecc2392b4af7b65198d6d5aab9
slavafleer <slavafleer@gmail.com>
2015-04-05 16:30:13 +0300

    added newUser data to Parse cloud

--- app/src/main/java/com/slava/ribbit/RibbitApplication.java
+++ app/src/main/java/com/slava/ribbit/RibbitApplication.java
@@ -3,7 +3,6 @@ package com.slava.ribbit;
 import android.app.Application;
 
 import com.parse.Parse;
-import com.parse.ParseObject;
 
 /**
  * Created by Slava on 05/04/2015.
@@ -17,9 +16,5 @@ public class RibbitApplication extends Application {
         Parse.enableLocalDatastore(this);
 
         Parse.initialize(this, "AsmLSbTlxqzs2rSLJ6L3CLfj1c7fdvhOzI25jlV5", "TTrioJSv7ai6v6ZZN73hwtpzoGXe4uVHsLH2TD8B");
-
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
     }
 }
--- app/src/main/java/com/slava/ribbit/SignUpActivity.java
+++ app/src/main/java/com/slava/ribbit/SignUpActivity.java
@@ -1,17 +1,82 @@
 package com.slava.ribbit;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 
 public class SignUpActivity extends ActionBarActivity {
+
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected EditText mEmail;
+    protected Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mUsername = (EditText) findViewById(R.id.usernameEditText);
+        mPassword = (EditText) findViewById(R.id.passwordEditText);
+        mEmail = (EditText) findViewById(R.id.emailEditText);
+        mSignUpButton = (Button) findViewById(R.id.signUpButton);
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+                String email = mEmail.getText().toString();
+
+                username = username.trim(); // removing spaces
+                password = password.trim();
+                email = email.trim();
+
+                if(username.isEmpty() || password.isEmpty() || email.isEmpty()){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this)
+                            .setMessage(getString(R.string.data_missed_alert))
+                            .setTitle(R.string.title_activity_sign_up)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    //Create new user
+                    ParseUser newUser = new ParseUser();
+                    newUser.setUsername(username);
+                    newUser.setPassword(password);
+                    newUser.setEmail(email);
+                    newUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                // Success
+                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this)
+                                        .setMessage(e.getMessage())
+                                        .setTitle(R.string.title_activity_sign_up)
+                                        .setPositiveButton(android.R.string.ok, null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 
 
