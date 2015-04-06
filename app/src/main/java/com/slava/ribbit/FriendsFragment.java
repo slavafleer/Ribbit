539ddeb1c8bb3c34e511805d760c48b4fe3aea9a
slavafleer <slavafleer@gmail.com>
2015-04-06 13:19:37 +0300

    added inbox and friends fragments to tabs

---
+++ app/src/main/java/com/slava/ribbit/FriendsFragment.java
@@ -0,0 +1,24 @@
+package com.slava.ribbit;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Slava on 06/04/2015.
+ */
+public class FriendsFragment extends ListFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // The first parameter is the layout ID that's used for this fragment.
+        // The second parameter is the container where the fragment will be displayed.
+        // This will be the view pager from main activity.
+        // The last parameter should be false whenever we're adding a fragment
+        // to an activity in code which we will do in a moment.
+        View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+        return rootView;
+    }
+}
